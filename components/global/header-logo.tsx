c5ebd3066deaad31f446cc8127323ce6235d1e0e
Ashmita-15 <144759739+Ashmita-15@users.noreply.github.com>
2024-10-26 23:01:04 +0530

    Update header-logo.tsx

--- components/global/header-logo.tsx
+++ components/global/header-logo.tsx
@@ -5,7 +5,7 @@ export const HeaderLogo = () => {
 	return (
 		<Link href="/">
 			<div className=" items-center hidden lg:flex">
-				<Image src="/logo.png" alt="Logo" width={30} height={30} className=" invert-0 dark:invert"/>
+				<Image src="/logo.svg" alt="Logo" width={30} height={30} className=" invert-0 dark:invert"/>
 				<p className="font-semibold text-2xl ml-2.5">CodeCache</p>
 			</div>
 		</Link>
